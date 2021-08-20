bb23e3944ce6a72219bed538c1f34d650fa2e733
Jafmatute <josueari20@gmail.com>
2021-08-20 12:32:22 -0600

    Lógica para el manejo de las tareas por hacer

--- 04-consola-iterativa/app.js
+++ 04-consola-iterativa/app.js
@@ -5,13 +5,14 @@ console.clear();
 
 // const { showMenu, pausa } = require("./helpers/mensajes.js");
 const { inquirerMenu, pausa } = require("./helpers/inquirer.js");
-
+const Tareas = require("./models/tareas");
 const main = async () => {
   let opt = "";
 
   do {
     opt = await inquirerMenu();
     console.log({ opt });
+
     await pausa();
     // if (opt !== "0") await pausa();
   } while (opt !== "0");
---
+++ 04-consola-iterativa/models/tareas.js
@@ -0,0 +1,13 @@
+/**
+ * {'uuid-12334444-55555-33': {id:12, desc:shshshs, compleadoEn: 12/32/12}}
+ * @format */
+
+class Tareas {
+  _listado = {};
+
+  constructor() {
+    this._listado = {};
+  }
+}
+
+module.exports = Tareas;
