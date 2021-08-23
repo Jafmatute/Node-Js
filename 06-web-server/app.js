3714d89aea09311d9c65b26849726971b5fb7cfa
Jafmatute <josueari20@gmail.com>
2021-08-23 11:50:26 -0600

    Express, servir contenido estático

--- 06-web-server/app.js
+++ 06-web-server/app.js
@@ -1,28 +1,20 @@
 /** @format */
 
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-http
-  .createServer((req, res) => {
-    // res.writeHead(200, { "Content-Type": "text/plain" });
-    // res.writeHead(200, { "Content-Type": "application/json" });
-    res.setHeader("Content-Disposition", "attachment; filename=lista.csv");
-    res.writeHead(200, { "Content-Type": "application/CSV" });
+//Midleware (servir contenido estático)
+app.use(express.static("public"));
 
-    const persona = {
-      id: 1,
-      nombre: "Josue",
-    };
+app.get("/menu", (req, res) => {
+  res.send("menu");
+});
 
-    // res.write(JSON.stringify(persona));
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/404.html");
+});
 
-    res.write("ID, nombre\n");
-    res.write("1, Maria\n");
-    res.write("2, Paola\n");
-    res.write("3, Francis\n");
-
-    res.end();
-  })
-  .listen(8080);
-
-console.log("Escuchando en el puerto", 8080);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
