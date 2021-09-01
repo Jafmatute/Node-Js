c68804ebdc918db7b91719dfe12b4cef4444e0b1
Jafmatute <josueari20@gmail.com>
2021-09-01 15:47:05 -0600

    Login de usuarios

--- 07-rest-server/controllers/auth.js
+++ 07-rest-server/controllers/auth.js
@@ -1,13 +1,48 @@
 /** @format */
 
 const { response } = require("express");
+const bcryptjs = require("bcryptjs");
 
-const login = (req, res = response) => {
-  const body = req.body;
-  res.json({
-    msg: "login",
-    body,
-  });
+const Usuario = require("../models/usuario");
+
+const login = async (req, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+    // Verificar si el e-mail existe
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res
+        .status(400)
+        .json({ msg: "Usuario / Password no son correctos " });
+    }
+    // Verificar usuario activo
+    if (!usuario.estado) {
+      return res
+        .status(400)
+        .json({ msg: "Usuario sin permisos, contacté con el administrador " });
+    }
+
+    // Verificar la contraseña
+    const verifyPass = bcryptjs.compareSync(password, usuario.password);
+    if (!verifyPass) {
+      return res
+        .status(400)
+        .json({ msg: "Usuario / Password no son correctos " });
+    }
+
+    //TODO: gener el JWT
+
+    res.json({
+      msg: "Exito al iniciar sesión",
+      password,
+      correo,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: "Hubo un error inesperado intente nuevamante" });
+  }
 };
 
 module.exports = {
