854063b86630d1f0b324f0ae9ea096bfd54efbc9
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-22 15:20:02 -0600

    Validar extensiones y helpers para subir archivos

--- 07-rest-server/controllers/uploads.js
+++ 07-rest-server/controllers/uploads.js
@@ -1,26 +1,16 @@
 /** @format */
 
-const path = require("path");
+const { subirArchivo } = require("../helpers/");
 
-const CargarArchivo = (req, res) => {
+const CargarArchivo = async (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     res.status(400).json({ msg: "No hay archivos en la peticion" });
     return;
   }
 
-  console.log("req.files >>>", req.files); // eslint-disable-line
+  const nombre = await subirArchivo(req.files);
 
-  const { archivo } = req.files;
-
-  const uploadPath = path.join(__dirname, "../uploads/" + archivo.name);
-
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
-
-    res.json({ msg: `archivo guardado en la ruta ${uploadPath}` });
-  });
+  res.json({ nombre });
 };
 
 module.exports = {
---
+++ 07-rest-server/helpers/subir-archivo.js
@@ -0,0 +1,41 @@
+/** @format */
+
+const { NOTIMP } = require("dns");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+const subirArchivo = (
+  files,
+  extensionesValidas = ["png", "jpg", "jpeg", "gif"],
+  carpeta = ""
+) => {
+  return new Promise((resolve, reject) => {
+    const { archivo } = files;
+    //Validar archivos
+    const nombreSplit = archivo.name.split(".");
+    const extension = nombreSplit[nombreSplit.length - 1];
+
+    //Validar Extension
+    if (!extensionesValidas.includes(extension)) {
+      return reject(
+        `La extensión ${extension} no es permitida, ${extesionesVal}`
+      );
+    }
+
+    const nombreTemp = uuidv4() + "." + extension;
+
+    const uploadPath = path.join(__dirname, "../uploads/", carpeta, nombreTemp);
+
+    archivo.mv(uploadPath, (err) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve(nombreTemp);
+    });
+  });
+};
+
+module.exports = {
+  subirArchivo,
+};
