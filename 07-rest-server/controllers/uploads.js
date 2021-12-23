a891d3c527817dbcd0574aca9abb126153eed93d
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-23 10:45:43 -0600

    Borrar archivos del sevidor

--- 07-rest-server/controllers/uploads.js
+++ 07-rest-server/controllers/uploads.js
@@ -1,5 +1,7 @@
 /** @format */
 
+const fs = require("fs");
+const path = require("path");
 const { subirArchivo } = require("../helpers/");
 const { Usuario, Producto } = require("../models/");
 
@@ -43,6 +45,21 @@ actualizarImagen = async (req, res) => {
     default:
       return res.status(500).json({ msg: "Se me olvido validar esto" });
       break;
+  }
+
+  //Limpiar imagenes previas
+
+  if (modelo.imagen) {
+    //Hay que borrar la imagen del servido5r
+    const pathImg = path.join(
+      __dirname,
+      "../uploads",
+      coleccion,
+      modelo.imagen
+    );
+    if (fs.existsSync(pathImg)) {
+      fs.unlinkSync(pathImg);
+    }
   }
 
   const nombre = await subirArchivo(req.files, undefined, coleccion);
