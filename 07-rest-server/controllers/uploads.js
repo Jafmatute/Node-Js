603af13a70701da75693e2b73035c85ddd241f10
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-23 15:11:42 -0600

    Carga de Imagenes a Cloudinary

--- 07-rest-server/controllers/uploads.js
+++ 07-rest-server/controllers/uploads.js
@@ -2,6 +2,9 @@
 
 const fs = require("fs");
 const path = require("path");
+const cloudinary = require("cloudinary").v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
+
 const { subirArchivo } = require("../helpers/");
 const { Usuario, Producto } = require("../models/");
 
@@ -68,6 +71,49 @@ const actualizarImagen = async (req, res) => {
 
   res.json(modelo);
 };
+const actualizarImagenCloudinary = async (req, res) => {
+  const { id, coleccion } = req.params;
+
+  let modelo;
+
+  switch (coleccion) {
+    case "usuarios":
+      modelo = await Usuario.findById(id);
+      if (!modelo)
+        return res
+          .status(400)
+          .json({ msg: `Nop existe el usuario con el id ${id}` });
+      break;
+    case "productos":
+      modelo = await Producto.findById(id);
+      if (!modelo)
+        return res
+          .status(400)
+          .json({ msg: `No existe el producto con el id ${id}` });
+      break;
+
+    default:
+      return res.status(500).json({ msg: "Se me olvido validar esto" });
+      break;
+  }
+
+  //Limpiar imagenes previas
+
+  if (modelo.imagen) {
+  }
+
+  const { tempFilePath } = req.files.archivo;
+  const resp = await cloudinary.uploader.upload(tempFilePath);
+  const { secure_url } = resp;
+  // const nombre = await subirArchivo(req.files, undefined, coleccion);
+  // modelo.imagen = nombre;
+  // await modelo.save();
+  // res.json(modelo);
+
+  modelo.imagen = secure_url;
+  await modelo.save();
+  res.json(modelo);
+};
 
 const mostrarImagen = async (req, res) => {
   const { id, coleccion } = req.params;
@@ -117,6 +163,7 @@ const mostrarImagen = async (req, res) => {
 
 module.exports = {
   actualizarImagen,
+  actualizarImagenCloudinary,
   cargarArchivo,
   mostrarImagen,
 };
--- 07-rest-server/routes/uploads.js
+++ 07-rest-server/routes/uploads.js
@@ -6,6 +6,7 @@ const {
   cargarArchivo,
   actualizarImagen,
   mostrarImagen,
+  actualizarImagenCloudinary,
 } = require("../controllers/uploads");
 const { coleccionesPermitidas } = require("../helpers");
 
@@ -37,7 +38,8 @@ router.put(
     ),
     validarCampos,
   ],
-  actualizarImagen
+  actualizarImagenCloudinary
+  // actualizarImagen
 );
 
 module.exports = router;
