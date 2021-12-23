9cb69b25d9133c0f867ee980fc88facc8e743a35
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-23 15:50:13 -0600

    Borrar imágenes de Cloudinary

--- 07-rest-server/controllers/uploads.js
+++ 07-rest-server/controllers/uploads.js
@@ -100,6 +100,22 @@ const actualizarImagenCloudinary = async (req, res) => {
   //Limpiar imagenes previas
 
   if (modelo.imagen) {
+    //Hay que borrar la imagen del servidor
+
+    const nombreArr = modelo.imagen.split("/");
+    const nombre = nombreArr[nombreArr.length - 1];
+    const [public_id] = nombre.split(".");
+    cloudinary.uploader.destroy(public_id);
+
+    // const pathImg = path.join(
+    //   __dirname,
+    //   "../uploads",
+    //   coleccion,
+    //   modelo.imagen
+    // );
+    // if (fs.existsSync(pathImg)) {
+    //   return res.sendFile(pathImg);
+    // }
   }
 
   const { tempFilePath } = req.files.archivo;
