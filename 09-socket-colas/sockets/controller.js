7c68579b992f8ca2356cc1de1a683a53e48353d3
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-25 14:44:49 -0600

    Socket Siguiente Ticket y Preparar pantalla de escritorio

--- 09-socket-colas/sockets/controller.js
+++ 09-socket-colas/sockets/controller.js
@@ -5,11 +5,13 @@ const TicketControl = require("../models/ticket-control");
 const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
-  socket.on("enviar-mensaje", (payload, callback) => {
-    const id = 123456789;
-    callback(id);
+  socket.emit("ultimo-ticket", ticketControl.ultimo);
 
-    socket.broadcast.emit("enviar-mensaje", payload);
+  socket.on("siguiente-ticket", (payload, callback) => {
+    const siguiente = ticketControl.siguiente();
+    callback(siguiente);
+
+    //TODO: Notificar que hay un nuevo ticket pendiente por asignar
   });
 };
 
