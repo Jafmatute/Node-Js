1f551da9269ddca707ffe36bff7a2f013bf83ce6
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-27 18:40:31 -0600

    Validar Token - Frontend

--- 10-socket-chat/public/js/chat.js
+++ 10-socket-chat/public/js/chat.js
@@ -1,3 +1,33 @@
 /** @format */
 
-const socket = io();
+const url = window.location.hostname.includes("localhost")
+  ? "http://localhost:8081/api/auth/"
+  : "https://restsnode.herokuapp.com/api/auth/";
+
+let usuario = null;
+let socket = null;
+
+const validarJWT = async () => {
+  const token = localStorage.getItem("token") || "";
+
+  if (token.length <= 10) {
+    window.location = "index.html";
+    throw new Error("No hay token en la petición");
+  }
+
+  const resp = await fetch(url, {
+    headers: { "x-token": token },
+  });
+
+  const { usuario: userDB, token: tokenDB } = await resp.json();
+  //   localStorage.setItem("token", tokenDB);
+  usuario = userDB;
+};
+
+const main = async () => {
+  await validarJWT();
+};
+
+main();
+
+// const socket = io();
