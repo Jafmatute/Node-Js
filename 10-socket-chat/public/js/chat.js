65279b2b4de14a8413df5a5ca544b51ed909ae34
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-29 15:07:55 -0600

    Mensajes privados

--- 10-socket-chat/public/js/chat.js
+++ 10-socket-chat/public/js/chat.js
@@ -53,8 +53,8 @@ const conectarSocket = async () => {
 
   socket.on("usuarios-activos", dibujarUsuarios);
 
-  socket.on("mensaje-privado", () => {
-    // TODO:
+  socket.on("mensaje-privado", (payload) => {
+    console.log("privado:", payload);
   });
 };
 
--- 10-socket-chat/sockets/controller.js
+++ 10-socket-chat/sockets/controller.js
@@ -19,6 +19,10 @@ const socketController = async (socket, io) => {
   chatMensajes.conectarUsuario(usuario);
   io.emit("usuarios-activos", chatMensajes.usuariosArr);
   socket.emit("recibir-mensajes", chatMensajes.ultimos10);
+
+  //Conectarlo a sala especial
+  socket.join(usuario.id); //global,socke.id, usuario.id
+
   // Limpiar cuando alguien se desconecta
   socket.on("disconnect", () => {
     chatMensajes.desconectarUsuario(usuario.id);
@@ -26,8 +30,13 @@ const socketController = async (socket, io) => {
   });
 
   socket.on("enviar-mensaje", ({ uid, mensaje }) => {
-    chatMensajes.enviarMensaje(usuario.id, usuario.nombre, mensaje);
-    io.emit("recibir-mensajes", chatMensajes.ultimos10);
+    if (uid) {
+      // Mensaje privado
+      socket.to(uid).emit("mensaje-privado", { de: usuario.nombre, mensaje });
+    } else {
+      chatMensajes.enviarMensaje(usuario.id, usuario.nombre, mensaje);
+      io.emit("recibir-mensajes", chatMensajes.ultimos10);
+    }
   });
 };
 
