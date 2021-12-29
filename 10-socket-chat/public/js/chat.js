c847bc684b2625fbf9bfa856c1e85f150907f9e5
Josue Ariel Flores Matute <josueari20@gmail.com>
2021-12-29 10:07:35 -0600

    HTML y JS para el chat de la app

--- 10-socket-chat/public/js/chat.js
+++ 10-socket-chat/public/js/chat.js
@@ -7,6 +7,13 @@ const url = window.location.hostname.includes("localhost")
 let usuario = null;
 let socket = null;
 
+// Referencias HTML
+const txtUid = document.querySelector("#txtUid");
+const txtMensaje = document.querySelector("#txtMensaje");
+const ulUsuarios = document.querySelector("#ulUsuarios");
+const ulMensajes = document.querySelector("#ulMensajes");
+const btnSalir = document.querySelector("#btnSalir");
+
 const validarJWT = async () => {
   const token = localStorage.getItem("token") || "";
 
@@ -28,10 +35,30 @@ const validarJWT = async () => {
 };
 
 const conectarSocket = async () => {
-  const socket = io({
+  socket = io({
     extraHeaders: {
       "x-token": localStorage.getItem("token"),
     },
+  });
+
+  socket.on("connect", () => {
+    console.log("socket online");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("socket ofline");
+  });
+
+  socket.on("recibir-mensajes", () => {
+    // TODO:
+  });
+
+  socket.on("usuarios-activos", () => {
+    // TODO:
+  });
+
+  socket.on("mensaje-privado", () => {
+    // TODO:
   });
 };
 
