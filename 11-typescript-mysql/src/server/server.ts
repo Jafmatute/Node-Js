d0180cb3d25422d4e6fa7b43edc7c4294b7c7386
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-20 17:25:06 -0600

    Desplegar el public folder

--- 11-typescript-mysql/src/server/server.ts
+++ 11-typescript-mysql/src/server/server.ts
@@ -1,4 +1,5 @@
 import express = require("express");
+import path = require('path');
 
 export default class Server {
 
@@ -14,8 +15,14 @@ export default class Server {
         return new Server(port);
     }
 
+    private publicFolder() {
+        const publicPath = path.resolve(__dirname, '../public');
+        this.app.use(express.static(publicPath));
+    }
+
     start(callback: () => void) {
         this.app.listen(this.port, callback);
+        this.publicFolder();
     }
 
 }
