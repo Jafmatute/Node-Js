c8c13f2609da0f2ecc001848bdf447761e54b50a
Josue Ariel Flores Matute <josueari20@gmail.com>
2022-01-20 17:03:58 -0600

    Configurando express en TypeScript

--- 11-typescript-mysql/dist/index.js
+++ 11-typescript-mysql/dist/index.js
@@ -1,4 +1,10 @@
 "use strict";
-console.log('====================================');
-console.log('Code Typescript');
-console.log('====================================');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = __importDefault(require("./server/server"));
+const server = server_1.default.init(3000);
+server.start(() => {
+    console.log(`Servidor corriendo en el puerto 3000`);
+});
